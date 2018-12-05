95c4f3536356ef5cf94a3c361fe3bcc03345908d
xSillusx <tkattanek00@gmail.com>
2018-12-05 16:37:12 +0100

    Init
    - created solution
    - created first golf

---
+++ CodeGolfs/CG_ColorOfPixel/Program.cs
@@ -0,0 +1,19 @@
+﻿// https://codegolf.stackexchange.com/questions/177020/color-of-a-pixel
+// 5.12.2018 TKa
+
+using System;
+using System.Drawing;
+
+namespace CG_ColorOfPixel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] splitInput = Console.ReadLine().Split(',');
+            Bitmap img = new Bitmap("test.png");
+            Color rgb = img.GetPixel(Convert.ToInt32(splitInput[0]), Convert.ToInt32(splitInput[1]));
+            Console.WriteLine(Math.Round((.1f/255)*rgb.R,3) + " , " + Math.Round((.1f / 255) * rgb.G, 3) + " , " + Math.Round((.1f / 255) * rgb.B, 3));
+        }
+    }
+}
